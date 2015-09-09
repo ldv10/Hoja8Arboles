50307178d2e551e80a062fafd6e58d6aa3233fb2
ldv10 <ninleo10@hotmail.com>
2015-09-08 22:55:13 -0600

    Create RootSuite.java

---
+++ RootSuite.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({IngresoHuffmanTest.class, NodoHTest.class, ArbolHTest.class, HojaHTest.class, HuffmanTest.class})
+public class RootSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+}
