2bc66c7b3239a843beae3d01adff27561f0ab214
ldv10 <ninleo10@hotmail.com>
2015-09-08 22:55:59 -0600

    Create NodoHTest.java

---
+++ NodoHTest.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class NodoHTest {
+    
+    public NodoHTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of getIzquierda method, of class NodoH.
+     */
+    @Test
+    public void testGetIzquierda() {
+        System.out.println("getIzquierda");
+        NodoH instance = null;
+        ArbolH expResult = null;
+       // ArbolH result = instance.getIzquierda();
+       // assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setIzquierda method, of class NodoH.
+     */
+    @Test
+    public void testSetIzquierda() {
+        System.out.println("setIzquierda");
+        ArbolH izquierda = null;
+       // NodoH instance = null;
+       // instance.setIzquierda(izquierda);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDerecha method, of class NodoH.
+     */
+    @Test
+    public void testGetDerecha() {
+        System.out.println("getDerecha");
+       // NodoH instance = null;
+        ArbolH expResult = null;
+        //ArbolH result = instance.getDerecha();
+       // assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      //  fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setDerecha method, of class NodoH.
+     */
+    @Test
+    public void testSetDerecha() {
+        System.out.println("setDerecha");
+        ArbolH derecha = null;
+       // NodoH instance = null;
+        //instance.setDerecha(derecha);
+        // TODO review the generated test code and remove the default call to fail.
+      //  fail("The test case is a prototype.");
+    }
+    
+}
