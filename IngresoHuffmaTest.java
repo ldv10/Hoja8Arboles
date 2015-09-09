fcdaa123a82dbf49b5a34c133cf7fd9101af8697
ldv10 <ninleo10@hotmail.com>
2015-09-08 22:57:19 -0600

    Create IngresoHuffmaTest.java

---
+++ IngresoHuffmaTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class IngresoHuffmanTest {
+    
+    public IngresoHuffmanTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of main method, of class IngresoHuffman.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+       // String[] args = null;
+       // IngresoHuffman.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+    
+}
